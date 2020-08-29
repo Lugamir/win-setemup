8ebc275172b4311e444ca16438d117057bcb5a9b
Tem <tem.dvlpr@gmail.com>
2020-08-29 21:00:01 +0200

    Add YAML parsing and config.yml

--- setemup.ps1
+++ setemup.ps1
@@ -1,3 +1,8 @@
+if (-not (Get-Module -Name "powershell-yaml")) {
+	Write-Host "No powershell-yaml module detected, installing..."
+    Install-Module powershell-yaml -Force
+}
+
 function IsCurrentUserAdmin {
     $user = [Security.Principal.WindowsIdentity]::GetCurrent()
     return (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
@@ -5,6 +10,12 @@ function IsCurrentUserAdmin {
 
 if (-not (IsCurrentUserAdmin)) {
 	Read-Host -Prompt "Needs to be executed in Admin PowerShell - [ENTER] to exit"
+	exit
+}
+
+$config = ConvertFrom-Yaml (Get-Content .\config.yml | Out-String)
+if (-not $config) {
+	Read-Host -Prompt "Parsed config.yml content empty, aborting - [ENTER] to exit"
 	exit
 }
 
