a108678eb900052dfa940293b4b984ed9cd0ea2c
Tem <tem.dvlpr@gmail.com>
2020-08-29 21:51:39 +0200

    Add welcome.txt and read welcome art

--- setemup.ps1
+++ setemup.ps1
@@ -1,17 +1,19 @@
 if (-not (Get-Module -Name "powershell-yaml")) {
 	Write-Host "No powershell-yaml module detected, installing..."
-    Install-Module powershell-yaml -Force
+	Install-Module powershell-yaml -Force
 }
 
 function IsCurrentUserAdmin {
-    $user = [Security.Principal.WindowsIdentity]::GetCurrent()
-    return (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+	$user = [Security.Principal.WindowsIdentity]::GetCurrent()
+	return (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 
 if (-not (IsCurrentUserAdmin)) {
 	Read-Host -Prompt "Needs to be executed in Admin PowerShell - [ENTER] to exit"
 	exit
 }
+
+Write-Host (Get-Content -Raw .\welcome.txt)
 
 $config = ConvertFrom-Yaml (Get-Content .\config.yml | Out-String)
 if (-not $config) {
