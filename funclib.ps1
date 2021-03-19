28f7fc8fd851d965f916ffca112987b9af8d36f1
Tem-Dev <tem.dvlpr@gmail.com>
2021-03-19 19:03:34 +0100

    Add function library with logging function

---
+++ funclib.ps1
@@ -0,0 +1,29 @@
+#Requires -Version 5
+
+Function Write-Log {
+    param (
+        [Parameter(Mandatory)] [string]$LogMessage,
+        [ValidateSet('Ok','Info','Warning','Error')] [string]$Severity = 'Info'
+    )
+
+    $LogMessage = "$(Get-Date -Format G); $Severity; $LogMessage"
+
+    if (-not $NoLog) {
+        if (-not $LogPath) {
+            $LogPath = "$PSScriptRoot\setemup.log"
+        }
+        $LogMessage | Out-File -FilePath $LogPath -Append
+    }
+
+    $Color = ""
+    switch ($Severity) {
+        'Ok' { $Color = "Green" }
+        'Warning' { $Color = "Yellow" }
+        'Error' { $Color = "Red" }
+        default { $Color = "White" }
+    }
+
+    if ($Silent -ne 'true') {
+        Write-Host -ForegroundColor $Color $LogMessage
+    }
+}
